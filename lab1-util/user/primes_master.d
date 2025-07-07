956f42505bcc07e5e932ef6f344853e05964a79b
0Ready0 <2812338122@qq.com>
2025-07-07 13:28:29 +0800

    feat: restore lab2-syscall to syscall branch

--- lab1-util/user/primes_master.d
+++
@@ -1,2 +0,0 @@
-user/primes_master.o: user/primes_master.c kernel/types.h kernel/stat.h \
- user/user.h
--- lab1-util/user/xargs.d
+++
@@ -1,2 +0,0 @@
-user/xargs.o: user/xargs.c kernel/types.h kernel/stat.h kernel/param.h \
- kernel/fs.h user/user.h
