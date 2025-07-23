cea09a153993eaa83ce4f985702e0b13271973d0
0Ready0 <2812338122@qq.com>
2025-07-23 17:25:16 +0800

    change: modify the discription

--- lab8-lock/kernel/buf.h
+++ lab8-lock/kernel/buf.h
@@ -2,7 +2,7 @@ struct buf {
   int valid;               // 数据是否有效（是否已从磁盘加载）
   int disk;                // 磁盘是否"拥有"缓冲区（是否正在进行 I/O 操作）
   uint dev;                // 设备号（如磁盘0/1）
-  uint blockno;            // 磁盘块号（512字节为单位）
+  uint blockno;            // 磁盘块号（1024字节为单位）
   struct sleeplock lock;   // 睡眠锁（保护缓冲区内容） 
   uint refcnt;             // 引用计数（当前使用者数量）
   struct buf *prev;        // LRU 链表前驱指针
